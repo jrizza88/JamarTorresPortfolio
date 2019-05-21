9dcfc62a2613efea122c518113ee1749060af823
Jamar Torres <jetorres88@gmail.com>
2019-05-21 11:37:48 -0400

    created Header and Footer components
    created Header and Footer components

--- jamar-portfolio-app/src/App.js
+++ jamar-portfolio-app/src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import { Route, withRouter, NavLink } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { Route, withRouter, NavLink } from "react-router-dom";
+import Header from "./components/Reusables/Header";
+import Footer from "./components/Reusables/Footer";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
       Jamar Torres Portfolio
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
