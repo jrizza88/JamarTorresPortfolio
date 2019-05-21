ea56c21aa9a2403a9761ac75325ef2192699a912
jrizza88 <jetorres88@gmail.com>
2019-05-21 00:14:22 -0400

    downloaded some dependecies to start project

---
+++ jamar-portfolio-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, withRouter, NavLink } from 'react-router-dom';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      Jamar Torres Portfolio
+    </div>
+  );
+}
+
+export default App;
