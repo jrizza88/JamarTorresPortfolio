defd2b4bfb95f2df67429e24662d5164718128d3
Jamar Torres <jetorres88@gmail.com>
2019-06-04 15:21:44 -0400

    updated header to be side nav based and downloaded styled components

--- jamar-portfolio-app/src/components/Reusables/Header.js
+++ jamar-portfolio-app/src/components/Reusables/Header.js
@@ -1,5 +1,38 @@
 import React from "react";
+import styled from "styled-components";
+import { Route, NavLink } from "react-router-dom";
 
-const Header = props => <div>Header</div>;
+const HeaderContainer = styled.div`
+  display: flex;
+  position: fixed;
+  flex-direction: column;
+  margin-top: 2.5%;
+  justify-content: flex-start;
+  align-items: flex-start;
+  align-self: flex-start;
+  border-right: 1px solid black;
+  width: 15%;
+  height: 100vh;
+`;
+
+const Header = props => (
+  <HeaderContainer>
+    <div>Home</div>
+    <div>About me</div>
+    <div>Projects</div>
+    <div>Resume</div>
+  </HeaderContainer>
+);
+
+// class Header extends React.Component {
+//   constructor (props) {
+//     super (props)
+//   }
+//   render () {
+//     <div>
+//       Header
+//     </div>
+//   }
+// }
 
 export default Header;
