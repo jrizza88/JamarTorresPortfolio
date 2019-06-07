696ec978ef2537377a7a764b045138a474faf2fb
jrizza88 <jetorres88@gmail.com>
2019-06-06 20:12:30 -0400

    created some styled components and arranged some of the components positions

--- jamar-portfolio-app/src/App.js
+++ jamar-portfolio-app/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from 'styled-components';
 import { Route, withRouter, NavLink } from "react-router-dom";
 import Header from "./components/Reusables/Header";
 import Footer from "./components/Reusables/Footer";
@@ -6,12 +7,39 @@ import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      Jamar Torres Portfolio
-      <Footer />
-    </div>
+      <Container>
+        <Header />
+          <BodyContainer>
+          <Headline>Jamar Torres <br />Web Developer</Headline>
+          <Footer />
+        </BodyContainer>
+      </Container>
   );
 };
+
+const Container = styled.section`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-between;
+max-width: 100%;
+width: 100%;
+`
+const BodyContainer = styled.section`
+display: flex;
+flex-direction: column;
+margin-left: 20%;
+max-width: 100%;
+width: 90%;
+`
+
+const Headline = styled.div`
+display: flex;
+position: relative;
+font-size: 3.5rem;
+justify-content: flex-start;
+width: 100%;
+border: solid green 1px;
+`
 
 export default withRouter(App);
--- jamar-portfolio-app/src/components/Reusables/Footer.js
+++ jamar-portfolio-app/src/components/Reusables/Footer.js
@@ -1,5 +1,19 @@
 import React from "react";
+import styled from 'styled-components';
 
-const Footer = props => <div>Footer</div>;
+const Footer = props => <FooterContainer>Footer</FooterContainer>;
+
+const FooterContainer = styled.footer`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    width: 100%;
+    position:absolute;
+    padding-bottom: 5%;
+    left:15%;
+    bottom:0;
+    right:0;
+    border: 2px solid purple;
+`
 
 export default Footer;
