28f6e07626de5c4de5188b495205789c17b5edbe
jrizza88 <jetorres88@gmail.com>
2019-06-09 20:20:23 -0400

    all routes set up with react router and created a resume component and added font family of choice

---
+++ jamar-portfolio-app/src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Posts extends React.Component{
+    state = {
+        posts: []
+    }
+    render(){
+        return (
+            <div>Posts</div>
+        )
+    }
+}
+
+export default Posts;
